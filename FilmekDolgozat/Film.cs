c7b1c8fb6cf8d040101162a16a9af1ac6b92ed82
peterffydominika <peterffy.dominika@gmail.com>
2025-05-28 12:14:16 +0200

    7.feladat

---
+++ FilmekDolgozat/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmekDolgozat
+{
+    class Film
+    {
+        public Film(string filmazon, string cim, int ev, string szines, string mufaj, int hossz)
+        {
+            Filmazon = filmazon;
+            Cim = cim;
+            Ev = ev;
+            Szines = szines;
+            Mufaj = mufaj;
+            Hossz = hossz;
+        }
+
+        public string Filmazon { get; set; }
+        public string Cim { get; set; }
+        public int Ev { get; set; }
+        public string Szines { get; set; }
+        public string Mufaj { get; set; }
+        public int Hossz { get; set; }
+    }
+}
